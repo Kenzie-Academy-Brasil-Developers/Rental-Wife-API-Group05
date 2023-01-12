7a9442be9b74b5910ea7b6e278395ca2648cc943
Alfredo <alfredofontinele08@gmail.com>
2023-01-12 17:37:51 -0300

    feat : creating users hired and employer services

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,76 @@
-import { deleteEmployerUser, updateAddressUser, updateEmployerUser } from "../controllers/users.controller";
+import { deleteEmployerUser, getEmployerUser, updateAddressUser, updateEmployerUser } from "../controllers/users.controller";
 import { Router } from "express";
+import {
+    deleteUserHired,
+    updateAddressUserHired,
+    updateUserHired,
+    updateServiceUserHired,
+    getAllHiredUsers,
+    getHiredUser,
+} from "../controllers/usersHired.controller";
 
 export const usersRouter = Router();
 
-usersRouter.patch("/employer/:id", updateEmployerUser);
-usersRouter.delete("/employer/:id", deleteEmployerUser);
-usersRouter.patch("/employer/:id/address", updateAddressUser);
+usersRouter.get("/hired", 
+    getAllHiredUsers
+);
+
+usersRouter.get("/hired/:id", 
+    // verificar usuario logado
+    // verificar se id do parametro é um hired
+    // verificar se o usuario logado é um employer
+    getHiredUser
+);
+
+usersRouter.patch("/hired", 
+    // verificar se o usuario logado
+    // verificar usuario logado é hired
+    // validateSchema()
+    updateUserHired
+);
+
+usersRouter.delete("/hired", 
+    // verificar se o usuario logado
+    // verificar usuario logado é hired
+    deleteUserHired
+);
+
+usersRouter.patch("/hired/address", 
+    // verificar se o usuario logado
+    // verificar usuario logado é hired
+    // validateSchema()
+    updateAddressUserHired
+);
+
+usersRouter.patch("/hired/services", 
+    // verificar se o usuario logado
+    // verificar usuario logado é hired
+    // verificar se o service existe 
+    updateServiceUserHired
+);
+
+usersRouter.get("/employer", 
+    // verificar se o usuario logado
+    // verificar usuario logado é employer
+    getEmployerUser
+);
+
+usersRouter.patch("/employer", 
+    // verificar se o usuario logado
+    // verificar usuario logado é employer
+    // validateSchema()
+    updateEmployerUser
+);
+
+usersRouter.delete("/employer", 
+    // verificar se o usuario logado
+    // verificar usuario logado é employer
+    deleteEmployerUser
+);
+
+usersRouter.patch("/employer/address", 
+    // verificar se o usuario logado
+    // verificar usuario logado é employer
+    // validateSchema()
+    updateAddressUser
+);
